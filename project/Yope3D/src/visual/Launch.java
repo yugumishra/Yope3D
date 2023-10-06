240f7a2955391638e411fe5a93bd8842411e5d3d
BuildTools <unconfigured@null.spigotmc.org>
2023-10-06 18:28:16 -0500

    Window Creation

--- project/Yope3D/src/visual/Launch.java
+++ project/Yope3D/src/visual/Launch.java
@@ -1,7 +1,15 @@
 package visual;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 public class Launch {
 	public static void main(String[] args) {
-		System.out.println("Initial project setup");
+		//get width and height from the toolkit of the current monitor
+		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+		
+		//initialize window using the width and height
+		Window window = new Window("Yope3D", screen.width, screen.height);
+		window.init();
 	}
 }
